a89f92231f9b2c399520d71f75e3c433623d3cb6
nathalieassis <nathaliesenac8@gmail.com>
2023-07-28 11:48:41 -0300

    projeto sistema padaria

---
+++ PadariaCarmel/frmLogin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PadariaCarmel
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bem vindo ao sistema.",
+                "Mensagem do sistema",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button3);
+        }
+    }
+}
